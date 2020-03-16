b0f637d329e410ef8226571e5c789c6268e78603
oowada takuji <jiinyo829@gmail.com>
2020-03-16 13:07:53 +0900

    Genreモデル作成とマイグレーション

--- nagano-cake/db/schema.rb
+++ nagano-cake/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_16_040452) do
+ActiveRecord::Schema.define(version: 2020_03_16_040717) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer "user_id"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 2020_03_16_040452) do
     t.integer "postal_code"
     t.text "address"
     t.string "addressee"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.boolean "is_active"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
