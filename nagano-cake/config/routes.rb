01dd560aaf821dcee265e7267d5684ecd8d08b1d
onokusuna <piroshki4315@gmail.com>
2020-03-16 06:39:17 +0000

    add_routes

--- nagano-cake/config/routes.rb
+++ nagano-cake/config/routes.rb
@@ -1,6 +1,25 @@
 Rails.application.routes.draw do
-  
   devise_for :admins
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'homes#top'
+  get 'home/about'
+  get 'users' => 'users#leave'
+  get 'orders' => 'orders#confirm'
+  get 'orders' => 'orders#complete'
+  get 'cart_items' => 'cart_items#all_destroy'
+  resources :users, only: [:show, :edit, :update, :destroy]
+  resources :items, only: [:index, :show]
+  resources :deliveries, only: [:index, :edit, :create, :update, :destroy]
+  resources :orders, only: [:index, :show, :new, :create]
+  resources :cart_items, only: [:index, :create, :update, :destroy]
+
+  namespace :admin do
+    root 'homes#top'
+    get 'users' => 'users#order_index'
+    resources :users, only: [:index, :show, :edit, :update]
+    resources :items, only: [:index, :show, :new, :edit, :create, :update]
+    resources :genres, only: [:index, :edit, :create, :update]
+    resources :orders, only: [:index, :edit, :update]
+  end
 end
