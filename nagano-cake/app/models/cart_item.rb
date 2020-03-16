251158d514c63631634d450e46ad5d0257299b3c
fudasatomi <fudasatomi.dmm@gmail.com>
2020-03-16 06:56:40 +0000

    modelファイル、関連付け追加

--- nagano-cake/app/models/cart_item.rb
+++ nagano-cake/app/models/cart_item.rb
@@ -1,3 +1,8 @@
 class CartItem < ApplicationRecord
 	belongs_to :user
+	belongs_to :item
+
+	validates :user_id ,presence: true
+    validates :item_id ,presence: true
+    validates :quantity ,presence: true
 end
--- nagano-cake/app/models/order_item.rb
+++ nagano-cake/app/models/order_item.rb
@@ -1,2 +1,11 @@
 class OrderItem < ApplicationRecord
+
+	belongs_to :item
+	belongs_to :order
+
+    validates :order_id ,presence: true
+    validates :item_id ,presence: true
+    validates :unit_price ,presence: true
+    validates :quantity ,presence: true
+    validates :status ,presence: true
 end
