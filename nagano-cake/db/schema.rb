7ff1af34b8b6f63512efb4fe0617642a832cd0d1
fudasatomi <fudasatomi.dmm@gmail.com>
2020-03-16 04:13:10 +0000

    User-model delete

--- nagano-cake/db/schema.rb
+++ nagano-cake/db/schema.rb
@@ -10,14 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_15_115412) do
-
-  create_table "cart_items", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "item_id"
-    t.integer "quantity"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
