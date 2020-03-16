2a04b7e164be9e0577a8e1998a16d6abfe04dae5
onokusuna <piroshki4315@gmail.com>
2020-03-16 18:22:49 +0900

    ジャンルコントローラ削除、adminアクション定義

--- nagano-cake/app/controllers/admins/items_controller.rb
+++ nagano-cake/app/controllers/admins/items_controller.rb
@@ -1,2 +1,26 @@
 class Admin::ItemsController < ApplicationController
+	def index
+
+	end
+
+	def show
+
+	end
+
+	def new
+
+	end
+
+	def edit
+
+	end
+
+	def create
+
+	end
+
+	def update
+
+	end
+
 end
