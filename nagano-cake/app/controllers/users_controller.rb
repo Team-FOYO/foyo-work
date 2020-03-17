80ef77d5a1202f02a468ab44d3a15f0c5818f515
fudasatomi <fudasatomi.dmm@gmail.com>
2020-03-17 06:43:03 +0000

    user/leave 枠組み

--- nagano-cake/app/controllers/users_controller.rb
+++ nagano-cake/app/controllers/users_controller.rb
@@ -24,3 +24,5 @@ class UsersController < ApplicationController
 	  def user_params
   			params.require(:user).permit(:first_name, :last_name,:first_name_kana,:last_name_kana,:email,:postcode,:address,:phone_number)
       end
+
+end
