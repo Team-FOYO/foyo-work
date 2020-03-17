5e9dec5843cc65692b115fcf6b0628eaf0a49fb2
fudasatomi <fudasatomi.dmm@gmail.com>
2020-03-17 06:25:56 +0000

    user/edit　枠組み

--- nagano-cake/app/controllers/users_controller.rb
+++ nagano-cake/app/controllers/users_controller.rb
@@ -5,11 +5,22 @@ class UsersController < ApplicationController
 	end
 
 	def edit
+		@user = User.find(params[:id])
 	end
 
 	def update
+	   @user = User.find(params[:id])
+  		if @user.update(user_params)
+  			redirect_to user_path(current_user), notice: "successfully updated user!"
+  		else
+  			render :edit
+  		end
 	end
 
 	def leave
 	end
-end
+
+	private
+	  def user_params
+  			params.require(:user).permit(:first_name, :last_name,:first_name_kana,:last_name_kana,:email,:postcode,:address,:phone_number)
+      end
