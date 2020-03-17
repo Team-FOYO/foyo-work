38dbfd6abd9b7d06ba21eb1730c216907923c626
onokusuna <piroshki4315@gmail.com>
2020-03-17 08:14:06 +0000

    ジャンル新規、一覧作成ほぼ完了

--- nagano-cake/app/controllers/admin/genres_controller.rb
+++ nagano-cake/app/controllers/admin/genres_controller.rb
@@ -1,6 +1,7 @@
 class Admin::GenresController < ApplicationController
 	def index
 		@genre = Genre.new
+		@genres = Genre.all
 	end
 
 	def edit
@@ -8,7 +9,13 @@ class Admin::GenresController < ApplicationController
 	end
 
 	def create
-
+		@genre = Genre.new(genre_params)
+		# @genre.admin_id = current_user_id
+		if @genre.save#, notice : '追加されました'
+			redirect_back(fallback_location: root_path)
+		else
+			redirect_back(fallback_location: root_path)
+		end
 	end
 
 	def update
@@ -16,8 +23,8 @@ class Admin::GenresController < ApplicationController
 	end
 	
 
-	# private
-	# 	def genre_params
-	# 		params.require(:genre).permit(:name)
-	# 	end
+	private
+		def genre_params
+			params.require(:genre).permit(:name)
+		end
 end
