585434121e9096ba7371c045b2dc6eb58f7ec1ed
fudasatomi <fudasatomi.dmm@gmail.com>
2020-03-17 07:48:34 +0000

    delivery/index のnew　フォーム

--- nagano-cake/app/controllers/deliveries_controller.rb
+++ nagano-cake/app/controllers/deliveries_controller.rb
@@ -1,9 +1,19 @@
 class DeliveriesController < ApplicationController
 	def index
+		@delivery = Delivery.new
+		@deliveries  = current_user.deliveries
 	end
 
-	def create
-	end
+  	def create
+  		@delivery = Delivery.new(delivery_params)
+  		@delivery.user_id = current_user.id
+    		if @delivery.save
+  				redirect_to deliveries_path, notice: "successfully created"
+  			else
+  		@deliveries  = current_user.deliveries
+  		render 'index'
+  		end
+  end
 
 	def edit
 	end
@@ -13,5 +23,10 @@ class DeliveriesController < ApplicationController
 
 	def destroy
 	end
-
+# private-----------------
+	private
+  		def delivery_params
+  			params.require(:delivery).permit(:postal_code, :address, :addressee)
+  		end
+# private-----------------
 end
