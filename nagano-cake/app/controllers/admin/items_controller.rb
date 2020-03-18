282686fcebd784d07fc9e08b4f40c28c96965b65
fudasatomi <fudasatomi.dmm@gmail.com>
2020-03-18 15:47:36 +0900

    新規登録画面にプルダウンを表示

--- nagano-cake/app/controllers/admin/items_controller.rb
+++ nagano-cake/app/controllers/admin/items_controller.rb
@@ -16,11 +16,21 @@ class Admin::ItemsController < ApplicationController
 	end
 
 	def create
-
+  		@item = Item.new(item_params)
+  		@item.genre_id =
+    	if @item.save #入力されたデータをdbに保存する。
+  			redirect_to items_path
+  		else
+  			render 'new'
+  		end
 	end
 
 	def update
 
 	end
 
+	private
+	def item_params
+		params.require(:item).permit(:genre_id,:name,:description, :image_id ,:no_tax , :is_active)
+	end
 end
