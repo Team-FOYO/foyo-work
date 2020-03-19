289dfa6dbf9c11fd1eac595614e0a583cac21260
fudasatomi <fudasatomi.dmm@gmail.com>
2020-03-19 21:12:53 +0900

    管理者側で会員情報の編集可能に

--- nagano-cake/app/controllers/admin/users_controller.rb
+++ nagano-cake/app/controllers/admin/users_controller.rb
@@ -18,7 +18,7 @@ class Admin::UsersController < ApplicationController
 	def update
 		@user = User.find(params[:id])
   		if @user.update(user_params)
-  			redirect_to user_path(current_user), notice: "successfully updated user!"
+  			redirect_to admin_user_path(@user), notice: "successfully updated user!"
   		else
   			render :edit
   		end
