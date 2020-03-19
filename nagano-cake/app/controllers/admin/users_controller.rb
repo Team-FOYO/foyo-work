da1581321dc6e94116659d8947ceec54270eab94
fudasatomi <fudasatomi.dmm@gmail.com>
2020-03-19 20:29:11 +0900

    管理者側、userのViewファイル作成

--- nagano-cake/app/controllers/admin/users_controller.rb
+++ nagano-cake/app/controllers/admin/users_controller.rb
@@ -1,6 +1,6 @@
 class Admin::UsersController < ApplicationController
 	def order_index
-
+		@user = User.find(params[:id])
 	end
 
 	def index
@@ -8,15 +8,24 @@ class Admin::UsersController < ApplicationController
 	end
 
 	def show
-
+		@user = User.find(params[:id])
 	end
 
 	def edit
-
+		@user = User.find(params[:id])
 	end
 
 	def update
-
+		@user = User.find(params[:id])
+  		if @user.update(user_params)
+  			redirect_to user_path(current_user), notice: "successfully updated user!"
+  		else
+  			render :edit
+  		end
 	end
-	
+
+	private
+	  def user_params
+  			params.require(:user).permit(:first_name, :last_name,:first_name_kana,:last_name_kana,:email,:postcode,:address,:phone_number, :is_active )
+      end
 end
