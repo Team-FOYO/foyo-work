b3567012605145808ddaaa63b186c8d5a8b4d0a9
吉田　充秀 <mauser_c96@train.ocn.ne.jp>
2020-03-19 16:33:19 +0900

    管理者がログアウトしたときに、管理者のサインイン画面へ、ヘッダーを編集

--- nagano-cake/config/application.rb
+++ nagano-cake/config/application.rb
@@ -16,4 +16,14 @@ module NaganoCake
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
   end
+
+  private
+
+  def after_sign_out_path_for(resource_or_scope)
+  	if resource_or_scope == :admin_admin
+  		new_admin_admin_session_path
+  	else
+  		admin_homes_top_path
+  	end
+  end
 end
