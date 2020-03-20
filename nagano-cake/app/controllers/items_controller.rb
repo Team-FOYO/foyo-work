270c68f70e330b1e91af5057149851c5c0e09fe4
onokusuna <piroshki4315@gmail.com>
2020-03-20 13:26:49 +0900

    ジャンル検索作成中

--- nagano-cake/app/controllers/items_controller.rb
+++ nagano-cake/app/controllers/items_controller.rb
@@ -2,11 +2,13 @@ class ItemsController < ApplicationController
 
 	def index
 		@items = Item.all
+		@genres = Genre.all
 	end
 
 	def show
 		@item = Item.find(params[:id])
 		@cart_item = CartItem.new
+		@genres = Genre.all
 	end
 
 
