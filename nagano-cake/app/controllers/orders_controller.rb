e691494c4b96135863184d24d6c413b530f1f92b
oowada takuji <jiinyo829@gmail.com>
2020-03-22 16:41:57 +0900

    注文確認ページ作成中

--- nagano-cake/app/controllers/orders_controller.rb
+++ nagano-cake/app/controllers/orders_controller.rb
@@ -2,25 +2,40 @@ class OrdersController < ApplicationController
 	def new
 		@user = current_user
 		@order = Order.new
+		@deliveries = Delivery.where(user_id: current_user.id)
 	end
 
 	def confirm
-		@order = Order.new(order_params)
+		@order = Order.where(params[:id])
 		@user = current_user.id
-		@cart_item = CartItem.where(user_id: current_user.id)
+		@cart_items = CartItem.where(user_id: current_user.id)
 		@delivery_charge = 800
 	end
 
     def create
-    	@user = current_user.id
-    	@order = Order.new(order_params)
-    	@order.user_id = current_user.id
-    	@order.save
-    	if @order.save
-    		redirect_to confirm_orders_path
-    	else
-    		redirect_to root_path
-    	end
+    	case params[:selected_btn]
+      		when  '会員住所'
+        		@order = Order.new(order_params)
+        		@order.user_id = current_user.id
+        		@order.postal_code = current_user.postcode
+        		@order.address = current_user.address
+        		@order.addressee = current_user.full_name.to_s
+        		@order.save
+        		redirect_to confirm_orders_path
+      		when  "配送先登録住所"
+        		@order = Order.new(order_params)
+        		@order.user_id = current_user.id
+        		@order.postal_code = Delivery.find(params[:order][:delivery]).postal_code
+        		@order.address = Shipping.find(params[:order][:delivery]).address
+        		@order.address_name = Shipping.find(params[:order][:delivery]).addressee
+        		@order.save
+        		redirect_to confirm_orders_path
+      		when  "新しい住所"
+        		@order = Order.new(order_params)
+        		@order.user_id = current_user.id
+        		@order.save
+        		redirect_to confirm_orders_path
+        	end
     end
 
 	def complete
@@ -32,8 +47,26 @@ class OrdersController < ApplicationController
 	def index
 	end
 
+	def update
+			@cart_items = CartItem.where(user_id: current_user.id)
+	        @cart_items.each do |cart_item|
+			    @order_item = OrderItem.new
+			    @order_item.order_id = @order.id
+			    @order_item.item_id = cart_item.item_id
+			    @order_item.quantity = cart_item.quantity
+			    @order_item.unit_price = cart_item.item.price
+			    @order_item.status = 1
+			end
+		 if @order_item.save
+	        @cart_items = CartItem.where(user_id: current_user.id)
+	        @cart_items.destroy_all
+	        @user = current_user
+	        redirect_to complete_orders_path
+	    end
+	end
+
 	private
 	def order_params
-    	params.permit(:address,:payment,)
+    	params.permit(:customer_id, :postcode, :address, :address_name,:delivery_charge, :charge, :payment, :status, :delivery)
   	end
 end
--- nagano-cake/app/models/order.rb
+++ nagano-cake/app/models/order.rb
@@ -6,12 +6,6 @@ class Order < ApplicationRecord
     has_many :deliveries
 
 	validates :user_id ,presence: true
-    validates :payment ,presence: true
-    validates :postal_code ,presence: true
-    validates :address ,presence: true
-    validates :addressee ,presence: true
-    validates :delivery_charge ,presence: true
-    validates :charge ,presence: true
     validates :status ,presence: true
 
     def total_address
