90ec9cedf12b56e8ab56471b1fef12a042c60347
吉田　充秀 <mauser_c96@train.ocn.ne.jp>
2020-03-25 13:14:34 +0900

    コントローラーのコメントアウト外し、アバウトのCSS完了

--- nagano-cake/app/controllers/admin/homes_controller.rb
+++ nagano-cake/app/controllers/admin/homes_controller.rb
@@ -1,5 +1,5 @@
 class Admin::HomesController < ApplicationController
-	#before_action :authenticate_admin_admin!
+	before_action :authenticate_admin_admin!
 
 	def top
 		@orders = Order.created_today
--- nagano-cake/app/controllers/deliveries_controller.rb
+++ nagano-cake/app/controllers/deliveries_controller.rb
@@ -1,5 +1,5 @@
 class DeliveriesController < ApplicationController
-  #before_action :authenticate_user!
+  before_action :authenticate_user!
 
 	def index
 		@delivery = Delivery.new
